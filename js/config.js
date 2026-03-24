a1c5aefb511c3f5f1fd0b7fc5155ceb4d33e415d
kyro <9013ms@gmail.com>
2026-03-24 10:16:42 +0530

    update config.js

--- js/config.js
+++ js/config.js
@@ -4,6 +4,6 @@
 const CONFIG = {
   API_BASE: 'https://apparent-jordanna-pixelart002-42e39ac6.koyeb.app/api/v1',
   // ⚠️  Replace with your Stripe publishable key
-  STRIPE_PK: 'pk_test_REPLACE_WITH_YOUR_STRIPE_PUBLISHABLE_KEY',
+  STRIPE_PK: 'pk_test_51LQQdRSDXqp6jmyTe96SuttCSgDD91Yu90PsGPLuw9liYziNa1TT0Yhi01fRdNuh5k656lM93wRYTjJZK7vzJBzL00FQaIQXYa',
   APP_NAME: 'Luviio',
 };
