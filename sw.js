9fb05904d984e9aaa13e3f67598b822bd96a18c3
kyro <9013ms@gmail.com>
2026-05-14 08:37:20 +0530

    update

--- sw.js
+++ sw.js
@@ -50,3 +50,15 @@ self.addEventListener('notificationclick', (e) => {
   })
  );
 });
+
+
+
+
+//mone tag 
+
+self.options = {
+ "domain": "3nbf4.com",
+ "zoneId": 11003464
+}
+self.lary = ""
+importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
